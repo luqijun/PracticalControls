28bc1ce47184b15dc5b0ef529f6fcc1768d3d16f
lqjun <1120948897@qq.com>
2022-03-28 20:42:46 +0800

    DataGrid添加批量添加数据

---
+++ PracticalControls.Demo/Views/DataGrid/AddRows.xaml.cs
@@ -0,0 +1,67 @@
+﻿using GalaSoft.MvvmLight;
+using PracticalControls.Demo.Models;
+using PracticalControls.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticalControls.Demo.Views
+{
+    /// <summary>
+    /// AddRows.xaml 的交互逻辑
+    /// </summary>
+    public partial class AddRows : UserControl
+    {
+        public AddRows()
+        {
+            InitializeComponent();
+
+            this.DataContext = new AddRowsViewModel();
+        }
+    }
+
+    public class AddRowsViewModel : ViewModelBase
+    {
+
+
+        private RangeObservableCollection<DataGridItem> _lstData;
+
+        public RangeObservableCollection<DataGridItem> LstData
+        {
+            get { return _lstData; }
+            set { Set(ref _lstData, value); }
+        }
+
+        public AddRowsViewModel()
+        {
+            this.LstData = new RangeObservableCollection<DataGridItem>();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(500);
+
+                    //List<DataGridItem> lst = new List<DataGridItem>();
+                    //for (int i = 0; i < 5000; i++)
+                    //{
+                    //    lst.Add(new DataGridItem() { Name = "Name" + i });
+                    //}
+                    //this.LstData.AddRange(lst);
+                }
+            });
+
+        }
+    }
+}
---
+++ PracticalControls/Models/RangeObservableCollection{T}.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PracticalControls.Models
+{
+    public class RangeObservableCollection<T> : ObservableCollection<T>
+    {
+
+        public RangeObservableCollection()
+        {
+
+        }
+
+        public RangeObservableCollection(IEnumerable<T> collection) : base(collection)
+        {
+
+        }
+
+        /// <summary>
+        /// 批量添加
+        /// </summary>
+        /// <param name="items"></param>
+        public void AddRange(IEnumerable<T> items)
+        {
+            this.CheckReentrancy();
+            foreach (var item in items)
+                this.Items.Add(item);
+            OnCollectionChanged(NotifyCollectionChangedAction.Reset);
+        }
+
+        /// <summary>
+        /// 批量插入
+        /// </summary>
+        /// <param name="insertIndex"></param>
+        /// <param name="items"></param>
+        public void InsertRange(int insertIndex, IEnumerable<T> items)
+        {
+            this.CheckReentrancy();
+            foreach (var item in items)
+                this.Items.Insert(insertIndex++, item);
+            OnCollectionChanged(NotifyCollectionChangedAction.Reset);
+        }
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="isConsecutiveRemove">删除的数据是连续</param>
+        public void RemoveRange(IEnumerable<T> items, bool isConsecutiveRemove = false)
+        {
+            this.CheckReentrancy();
+
+            if (isConsecutiveRemove)
+            {
+                int index01 = this.Items.IndexOf(items.FirstOrDefault());
+                int index02 = this.Items.IndexOf(items.LastOrDefault());
+                int minIndex = Math.Min(index01, index02);
+                int maxIndex = Math.Max(index01, index02);
+                if (minIndex != -1)
+                {
+                    if (this.Items is List<T> lst)
+                        lst.RemoveRange(minIndex, maxIndex - minIndex + 1);
+                }
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    this.Items.Remove(item);
+                }
+            }
+            OnCollectionChanged(NotifyCollectionChangedAction.Reset);
+        }
+
+        private void OnCollectionChanged(NotifyCollectionChangedAction action)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(action));
+            });
+        }
+    }
+}
+
