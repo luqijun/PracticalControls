25f085e9588fe20ec7e9679dbbdd474a91696f56
lqjun <lu.qijun@zd-med.com>
2022-11-27 11:19:47 +0800

    Add FlowDocument test

---
+++ PracticalControls.Demo/Views/FlowDocument/FlowDocument.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticalControls.Demo.Views
+{
+    /// <summary>
+    /// Interaction logic for FlowDocumentView.xaml
+    /// </summary>
+    public partial class FlowDocument : UserControl
+    {
+        public FlowDocument()
+        {
+            InitializeComponent();
+        }
+    }
+}
